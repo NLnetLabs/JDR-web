723278f91a365e71f44d5c02f6262c228c4ab279
Luuk Hendriks <mail@luukhendriks.eu>
2021-02-17 10:01:22 +0100

    rearrange parts of the graph
    WiP commit, several things in here:
    - reduce font sizes and space in between nodes of the graph
    - max-height on the Search version of the graph
    - reduce padding/margin around tabs in the Search page
    - on the Repo view, move warnings icons upwards so the name of the repo
      can be written out in full
    - in the Repo view, split the error counter in self+children

--- src/components/TreeChart.vue
+++ src/components/TreeChart.vue
@@ -9,7 +9,20 @@
         }"
       >
         <div :class="{ node: true }">
-          <div class="leaf" :class="Array.isArray(treeData.class) ? treeData.class : []">
+        <el-row>
+          <el-col :span="8"
+            ><el-tooltip
+              class="item"
+              effect="dark"
+              :content="treeData.additionalInfo"
+              placement="left"
+              v-if="treeData.additionalInfo"
+              ><div :class="treeData.additionalInfoSeverity" style="text-align: right">
+                <i class="el-icon-discover"></i></div></el-tooltip
+            >&nbsp;</el-col
+          >
+        <el-col :span="8"
+          ><div class="leaf" :class="Array.isArray(treeData.class) ? treeData.class : []">
             <el-tooltip
               class="item"
               effect="dark"
@@ -21,51 +34,60 @@
                 <img :src="treeData.image_url" />
               </div>
             </el-tooltip>
-
-            <div>
+            </div>
+          </el-col>
+          <el-col :span="8"
+            ><el-tag
+              size="mini"
+              type="warning"
+              v-if="$route.name == 'repositories' && treeData.warnings"
+              :title="treeData.warnings + ' warnings, see overview below'"
+              >{{ treeData.warnings }}</el-tag
+            >
+            <el-tag
+              size="mini"
+              type="danger"
+              v-if="$route.name == 'repositories' && (treeData.errors_self || treeData.errors_children)"
+              :title="(treeData.errors_self ? (treeData.errors_self + ' errors in self') : ''  + treeData.errors_children ?  (treeData.errors_children + ' errors in children') : '') + ', see overview below'"
+              >{{ treeData.errors_self || '0' }} {{ treeData.errors_children ?
+              '+' : '' }} {{ treeData.errors_children }}</el-tag
+            ></el-col
+          >
+            </el-row>
               <div class="name" @mouseup="$emit('click-node', treeData)">
-                <div class="name-container">
+                <div :class="[{clickable: treeData.name !== 'root'}, name-container]">
                   <el-row>
-                    <el-col :span="3"
-                      ><el-tooltip
-                        class="item"
-                        effect="dark"
-                        :content="treeData.additionalInfo"
-                        placement="left"
-                        v-if="treeData.additionalInfo"
-                        ><div :class="treeData.additionalInfoSeverity">
-                          <i class="el-icon-discover"></i></div></el-tooltip
-                      >&nbsp;</el-col
-                    >
-                    <el-col :span="18"
+                    <el-col :span="($route.name == 'search') ? 21 : 24"
                       ><el-tooltip
                         class="item"
                         effect="dark"
                         :content="treeData.name"
                         placement="right"
-                        ><div v-text-middle-ellipsis="4">{{ treeData.name }}</div></el-tooltip
+                        ><div v-if="$route.name == 'search' || Array.isArray(treeData.siblings) && treeData.siblings.length" v-text-middle-ellipsis="10">{{ treeData.name }}</div>
+                        <div v-else>{{ treeData.name }}</div></el-tooltip
                       ></el-col
                     >
-                    <el-col :span="3"
-                      >&nbsp;<el-tag
-                        size="mini"
-                        type="warning"
-                        v-if="treeData.warnings"
-                        :title="treeData.warnings + ' warnings'"
-                        >{{ treeData.warnings }}</el-tag
-                      >
-                      <el-tag
-                        size="mini"
-                        type="danger"
-                        v-if="treeData.errors"
-                        :title="treeData.errors + ' errors'"
-                        >{{ treeData.errors }}</el-tag
-                      ></el-col
-                    >
+                    <el-col
+                      v-if="$route.name == 'search'"
+                      :span=3
+                      ><el-tag
+                          size="mini"
+                          type="warning"
+                          v-if="treeData.warnings"
+                          :title="treeData.warnings + ' warnings'"
+                          >{{ treeData.warnings }}</el-tag
+                          >
+                          <el-tag
+                            size="mini"
+                            type="danger"
+                            v-if="treeData.errors"
+                            :title="treeData.errors + ' errors'"
+                            >{{ treeData.errors }}</el-tag
+                          >
+                    </el-col>
                   </el-row>
                 </div>
               </div>
-            </div>
 
             <template v-if="Array.isArray(treeData.siblings) && treeData.siblings.length">
               <div
@@ -77,6 +99,7 @@
                   <div class="name leafname" @click="$emit('click-node', mate)">
                     <div class="name-container">
                       <el-row>
+                      <!--
                         <el-col :span="3"
                           ><el-tooltip
                             class="item"
@@ -88,17 +111,18 @@
                               <i class="el-icon-discover"></i></div></el-tooltip
                           >&nbsp;</el-col
                         >
-                        <el-col :span="18"
+                        -->
+                        <el-col :span="21"
                           ><el-tooltip
                             class="item"
                             effect="dark"
                             :content="mate.name"
                             placement="right"
-                            ><div v-text-middle-ellipsis="4">{{ mate.name }}</div></el-tooltip
+                            ><div v-text-middle-ellipsis="10">{{ mate.name }}</div></el-tooltip
                           ></el-col
                         >
                         <el-col :span="3"
-                          >&nbsp;<el-tag
+                          ><el-tag
                             size="mini"
                             type="warning"
                             v-if="mate.warnings"
@@ -120,7 +144,6 @@
               </div>
             </template>
           </div>
-        </div>
         <div
           class="extend_handle"
           v-if="Array.isArray(treeData.children) && treeData.children.length > 4"
@@ -301,12 +324,13 @@ td {
   margin: 0 1em;
   box-sizing: border-box;
   text-align: center;
+  max-width: 10em;
 
   .leaf {
     position: relative;
     display: inline-block;
     z-index: 2;
-    width: 10em;
+    /*width: 7em;*/
     overflow: hidden;
 
     .avatar {
@@ -330,20 +354,19 @@ td {
       width: 100%;
       height: 100%;
     }
+  }
 
     .name {
       height: 1.6em;
       line-height: 1.6em;
       overflow: hidden;
-      font-size: 0.8rem;
-      /* width: 100%; */
+      font-size: 0.7rem;
+      /*width: 100%;*/
       width: 9rem;
-      min-width: 9rem;
-      padding-right: 1rem;
+      min-width: 7rem;
+      /* padding-right: 1rem; */
       white-space: nowrap;
-      padding-left: 0.5rem;
     }
-  }
 
   &.hasMate::after {
     content: "";
@@ -414,7 +437,7 @@ td {
 }
 
 .name-container {
-  width: 8rem;
+  /*width: 8rem;*/
   margin-left: auto;
   margin-right: auto;
 }
@@ -430,4 +453,11 @@ td {
 .info {
   color: #bbb;
 }
+
+.el-tag--mini {
+  font-size: 0.6rem;
+  padding: 0 0.1rem;
+  cursor: default;
+}
+
 </style>
